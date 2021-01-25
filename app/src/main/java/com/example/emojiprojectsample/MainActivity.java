c7f8849bebe202e873e17633900c3168b45f425a
Ayush Rastogi <ayush.rastogi@daalchini.co.in>
2021-01-25 14:52:10 +0530

    Added All required files

---
+++ app/src/main/java/com/example/emojiprojectsample/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.emojiprojectsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import hani.momanii.supernova_emoji_library.Actions.EmojIconActions;
+import hani.momanii.supernova_emoji_library.Helper.EmojiconEditText;
+import hani.momanii.supernova_emoji_library.Helper.EmojiconTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    CheckBox mCheckBox;
+    EmojiconEditText emojiconEditText;
+    EmojiconTextView textView;
+    ImageView emojiImageView;
+    ImageView submitButton;
+    View rootView;
+    EmojIconActions emojIcon;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rootView = findViewById(R.id.root_view);
+        emojiImageView =  findViewById(R.id.emoji_btn);
+        submitButton =  findViewById(R.id.submit_btn);
+        mCheckBox =  findViewById(R.id.use_system_default);
+        emojiconEditText =  findViewById(R.id.emojicon_edit_text);
+        textView =  findViewById(R.id.textView);
+        emojIcon = new EmojIconActions(this, rootView, emojiconEditText, emojiImageView);
+        emojIcon.ShowEmojIcon();
+        emojIcon.setIconsIds(R.drawable.ic_action_keyboard, R.drawable.smiley);
+
+        emojIcon.setKeyboardListener(new EmojIconActions.KeyboardListener() {
+            @Override
+            public void onKeyboardOpen() {
+                Toast.makeText(getApplicationContext(), "Keyboard opened !", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onKeyboardClose() {
+                Toast.makeText(getApplicationContext(), "Keyboard closed !", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Log.d(TAG, "My boolean value is = "+b);
+                emojIcon.setUseSystemEmoji(b);
+                textView.setUseSystemDefault(b);
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newText = emojiconEditText.getText().toString();
+                textView.setText(newText);
+            }
+        });
+
+    }
+}
